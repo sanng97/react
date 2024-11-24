ed5c4979551c6d950ddc7035d4a66717215fd6b9
sanng97@naver.com <sanng97@naver.com>
2024-11-25 04:00:48 +0900

    1125 003

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import ImportComponent from "./R003_ImportComponent"
+
+
+
+function App () {
+  return (
+    <div>
+      <h1>start React 200!</h1>
+      <p> HTML 적용하기</p>
+      <ImportComponent></ImportComponent>
+    </div>
+
+  );
+}
+export default App;
---
+++ client/src/R003_ImportComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class R003_ImportComponent extends Component {
+    render (){
+        return (
+            <h2>[THIS IS IMPORTED COMPONENT]</h2> // return된 html 코드를 render() 함수를 사용해 화면에 표시한다.
+        )
+    }
+}
+
+export default R003_ImportComponent;
